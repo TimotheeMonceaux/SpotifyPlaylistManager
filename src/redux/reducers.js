b9c8dd77520b68f0acba2ae47e8f987dd7ce5ad0
Timothée Monceaux <timothee.monceaux@shift-technology.com>
2019-03-15 15:43:24 +0100

    add action ForceState and implement it in reducer

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -3,38 +3,56 @@ import { ActionType, LibrarySort } from './actions';
 
 
 const clientId = (clientId = "", action) => {
+    if (action.type === ActionType.FORCE_STATE)
+        return action.newState.clientId;
+
     if (action.type === ActionType.ADD_CLIENT_ID)
         return action.clientId;
+
     return clientId;
 };
 
 const userToken = (userToken = "", action) => {
+    if (action.type === ActionType.FORCE_STATE)
+        return action.newState.userToken;
+
     if (action.type === ActionType.ADD_USER_TOKEN)
         return action.userToken;
+
     return userToken;
 };
 
 const userProfile = (userProfile = {}, action) => {
+    if (action.type === ActionType.FORCE_STATE)
+        return action.newState.userProfile;
+
     if (action.type === ActionType.ADD_USER_PROFILE) {
         return action.userProfile;}
+
     return userProfile;
 }
 
 const userPlaylists = (userPlaylists = [], action) => {
+    if (action.type === ActionType.FORCE_STATE)
+        return action.newState.userPlaylists;
+
     if (action.type === ActionType.ADD_USER_PLAYLISTS)
         return action.userPlaylists.map((p) => Object.assign({}, p, {enabled: true, tracks: {}}));
+
     if (action.type === ActionType.TOGGLE_USER_PLAYLIST) {
         let index = userPlaylists.findIndex((p) => p.id === action.playlistId);
         return [...userPlaylists.slice(0,index),
                 Object.assign({}, userPlaylists[index], {enabled: !userPlaylists[index].enabled}),
                 ...userPlaylists.slice(index+1, userPlaylists.length)];
     }
+
     if (action.type === ActionType.APPEND_PLAYLIST_TRACKS) {
         let index = userPlaylists.findIndex((p) => p.id === action.playlistId);
         return [...userPlaylists.slice(0,index),
                 Object.assign({}, userPlaylists[index], {tracks: {...userPlaylists[index].tracks, ...action.tracks.map(t => t.track.id).reduce((o, id) => {o[id] = true; return o;}, {})}})
                 ,...userPlaylists.slice(index+1, userPlaylists.length)];
     }
+
     if (action.type === ActionType.ADD_PLAYLIST_TRACK) {
         let index = userPlaylists.findIndex((p) => p.id === action.playlistId);
         let tracks = Object.assign({}, userPlaylists[index].tracks)
@@ -43,6 +61,7 @@ const userPlaylists = (userPlaylists = [], action) => {
             Object.assign({}, userPlaylists[index], {tracks: tracks})
             ,...userPlaylists.slice(index+1, userPlaylists.length)];
     }
+
     if (action.type === ActionType.DELETE_PLAYLIST_TRACK) {
         let index = userPlaylists.findIndex((p) => p.id === action.playlistId);
         let tracks = Object.assign({}, userPlaylists[index].tracks)
@@ -51,21 +70,30 @@ const userPlaylists = (userPlaylists = [], action) => {
             Object.assign({}, userPlaylists[index], {tracks: tracks})
             ,...userPlaylists.slice(index+1, userPlaylists.length)];
     }
+
     return userPlaylists;
 }
 
 const library = (library = [], action) => {
+    if (action.type === ActionType.FORCE_STATE)
+        return action.newState.library;
+
     if (action.type === ActionType.APPEND_LIBRARY_TRACKS)
         return library.concat(action.tracks);
+
     return library;
 }
 
 const librarySort = (librarySort = {sort: LibrarySort.DEFAULT, asc: 1}, action) => {
+    if (action.type === ActionType.FORCE_STATE)
+        return action.newState.librarySort;
+
     if (action.type === ActionType.CHANGE_LIBRARY_SORT && action.librarySort !== LibrarySort.DEFAULT) {
         if (librarySort.sort === action.librarySort)
             return Object.assign({}, librarySort, {asc: -1 * librarySort.asc});
         return {sort: action.librarySort, asc: 1};
     }
+
     return librarySort;
 }
 
